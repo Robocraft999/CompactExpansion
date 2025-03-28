bd35d73f13d73195395f327703016b95a3380288
Robocraft999 <59525378+Robocraft999@users.noreply.github.com>
2025-03-28 01:25:10 +0100

    fixed items only dropping in the center chunk

---
+++ src/main/java/com/robocraft999/compactexpansion/mixin/RoomEventHandlerMixin.java
@@ -0,0 +1,38 @@
+package com.robocraft999.compactexpansion.mixin;
+
+import dev.compactmods.machines.api.dimension.CompactDimension;
+import dev.compactmods.machines.room.RoomEventHandler;
+import dev.compactmods.machines.room.data.CompactRoomData;
+import dev.compactmods.machines.room.exceptions.NonexistentRoomException;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.ChunkPos;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.Vec3;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(RoomEventHandler.class)
+public class RoomEventHandlerMixin {
+
+    @Inject(
+            at = @At("HEAD"),
+            method = "positionInsideRoom",
+            cancellable = true,
+            remap = false
+    )
+    private static void onPositionInsideRoom(Entity entity, Vec3 target, CallbackInfoReturnable<Boolean> cir){
+        Level level = entity.level();
+        if (!level.dimension().equals(CompactDimension.LEVEL_KEY)) {
+            cir.setReturnValue(false);
+        } else if (level instanceof ServerLevel compactDim) {
+            CompactRoomData intern = CompactRoomData.get(compactDim);
+            cir.setReturnValue(intern.streamRooms().anyMatch(room -> room.getRoomBounds().contains(target)));
+        } else {
+            cir.setReturnValue(false);
+        }
+        cir.cancel();
+    }
+}
